db41ed170acdd24168c78a6f8de7ba7983690b43
kadircanincekalan <kadircanincekalan@gmail.com>
2021-10-07 15:30:35 +0300

    study.

---
+++ app.js
@@ -0,0 +1,29 @@
+// var quantity;
+// console.log(quantity);
+
+// quantity = 3;
+// console.log(quantity);
+
+// var a = 5;
+// console.log(a);
+
+/* var a = 10;
+console.log('a baslangic :' , a);
+
+{
+    let b = 5 
+    console.log('scope içinde b:' ,b)
+} */
+
+/* console.log(c);
+var c = 23;
+console.log(c); */
+/* 
+console.log(d);
+let d =25; */
+
+/* const x = 'Hello'
+console.log(x);
+const x = 'Hello world' */
+
+
